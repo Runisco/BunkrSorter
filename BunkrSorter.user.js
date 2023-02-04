5afdd92907a6bd2072888fd7df623e72f21c0908
Runisco <runisco@gmail.com>
2023-02-04 23:40:47 +0100

    2.0

--- BunkrSorter.user.js
+++ BunkrSorter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BunkrSorter
 // @namespace    https://github.com/runisco
-// @version      1.4
+// @version      2.0
 // @updateURL    https://github.com/Runisco/BunkrSorter/raw/main/BunkrSorter.user.js
 // @downloadURL  https://github.com/Runisco/BunkrSorter/raw/main/BunkrSorter.user.js
 // @supportURL   https://github.com/Runisco/BunkrSorter/issues
@@ -20,8 +20,10 @@
 var sortButton = $('<a href="#" class="sort" id="startSort">sort items</a>');
 sortButton.insertAfter($('p.subtitle'));
 $('#startSort').css({'margin-left':'10px'});
+var sortButtonFriends = $('<li><a href="#" class="sort" id="startSortFriends">sort items</a></li>');
+sortButtonFriends.insertAfter($('div.friends'))
 
-var debug = false
+var debug = true
 var debugOnlyOne = false
 
 $('#startSort').click(function(){
@@ -69,5 +71,53 @@ $('#startSort').click(function(){
 
     for (let i=0; i < sortedItems.length; i++){
         $('div#table').append(sortedItems[i][0]);
+    }
+})
+
+$('#startSortFriends').click(function(){
+    var items = [];
+    $('div.overflow-hidden').each(function(e){
+        let item = []
+        let size, sizeMultiplier;
+        item.push($(this));
+
+        if (debug || debugOnlyOne){console.log($(this))}
+        $(this).remove();
+
+        let sizeInfo = $(this).find('p:eq(1)').text();
+        let sizeSplit = sizeInfo.split(" ");
+        size = parseFloat(sizeSplit[0]);
+        if (debug || debugOnlyOne) {console.log(sizeInfo + " " + size)}
+
+        let sizeMultiplierDeterminer = sizeSplit[1]
+        if (sizeMultiplierDeterminer == "KiB"){
+            sizeMultiplier = (1/1024);
+        } else if (sizeMultiplierDeterminer == "KB"){
+            sizeMultiplier = (1/1024);
+        } else if (sizeMultiplierDeterminer == "kB"){
+            sizeMultiplier = (1/1024);
+        }else if (sizeMultiplierDeterminer == "MiB"){
+            sizeMultiplier = 1;
+        } else if (sizeMultiplierDeterminer == "MB"){
+            sizeMultiplier = 1;
+        } else if (sizeMultiplierDeterminer == "GiB"){
+            sizeMultiplier = 1024;
+        } else if (sizeMultiplierDeterminer == "GB"){
+            sizeMultiplier = 1024;
+        }
+        item.push(size * sizeMultiplier);
+        if (debug || debugOnlyOne) {console.log(size + " = " + size * sizeMultiplier)}
+
+        items.push(item);
+        $(this).remove();
+        debugOnlyOne = false;
+    });
+
+    var sortedItems = items.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+
+    for (let i=0; i < sortedItems.length; i++){
+        $('div.grid').append(sortedItems[i][0]);
     }
 })
