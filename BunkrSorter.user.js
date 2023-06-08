a500cd52c0b01278103b9de5f803562fb1c10224
Runisco <runisco@gmail.com>
2023-06-08 02:34:18 +0200

    Update BunkrSorter.user.js

--- BunkrSorter.user.js
+++ BunkrSorter.user.js
@@ -11,6 +11,7 @@
 // @match        https://bunkr.ru/a/*
 // @match        https://bunkr.su/a/*
 // @match        https://bunkr.la/a/*
+// @match        https://bunkr.su/a/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bunkr.is
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
 // @grant        none
