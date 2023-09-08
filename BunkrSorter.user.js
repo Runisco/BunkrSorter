7100be16156f592216c675e07f8a3fc4766454e5
Runisco <runisco@gmail.com>
2023-09-09 00:59:42 +0200

    Update BunkrSorter.user.js

--- BunkrSorter.user.js
+++ BunkrSorter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         BunkrSorter
+// @name         BunkrSortertest
 // @namespace    https://github.com/runisco
-// @version      2.2
+// @version      3.0
 // @updateURL    https://github.com/Runisco/BunkrSorter/raw/main/BunkrSorter.user.js
 // @downloadURL  https://github.com/Runisco/BunkrSorter/raw/main/BunkrSorter.user.js
 // @supportURL   https://github.com/Runisco/BunkrSorter/issues
@@ -23,10 +23,10 @@ var sortButton = $('<a href="#" class="sort" id="startSort">sort items</a>');
 sortButton.insertAfter($('p.subtitle'));
 $('#startSort').css({'margin-left':'10px'});
 // $('#startlistSort').css({'margin-left':'10px'});
-var sortButtonFriends = $('<li><a href="#" class="sort" id="startSortFriends">sort items</a></li>');
-var listButtonFriends = $('<li><a href="#" class="listSort" id="startlistSort">sort & list items</a></li>');
-sortButtonFriends.insertAfter($('div.friends'))
-listButtonFriends.insertAfter(sortButtonFriends)
+var sortButtonFriends = $('<a id="startSortFriends" class="py-2 px-4 rounded inline-flex items-center justify-center" href="#" style="background: #5253F6;color: #F6F652;font-weight: bold;">Sort Items</a>');
+// var listButtonFriends = $('<li><a href="#" class="listSort" id="startlistSort">sort & list items</a></li>');
+sortButtonFriends.insertBefore($('a#statsLink'))
+// listButtonFriends.insertAfter(sortButtonFriends)
 
 var debug = true
 var debugOnlyOne = false
@@ -146,72 +146,72 @@ $('#startSortFriends').click(function(){
     }
 })
 
-$('#startlistSort').click(function(){
-    if (debug || debugOnlyOne){console.log("New startlistSort clicked")}
+// $('#startlistSort').click(function(){
+//     if (debug || debugOnlyOne){console.log("New startlistSort clicked")}
 
-    var items = [];
+//     var items = [];
 
-    if ($('div.overflow-hidden').length != 0){
-        identifiedClass = 'div.overflow-hidden'
-    } else if ($('div.grid-images_box').length != 0) {
-        identifiedClass = 'div.grid-images_box'
-    }
+//     if ($('div.overflow-hidden').length != 0){
+//         identifiedClass = 'div.overflow-hidden'
+//     } else if ($('div.grid-images_box').length != 0) {
+//         identifiedClass = 'div.grid-images_box'
+//     }
 
-    if ($('div.grid').length != 0){
-        identifiedContainer = 'div.overflow-hidden'
-    } else if ($('div.grid-images_box').length != 0) {
-        identifiedContainer = 'div.grid-images'
-    }
+//     if ($('div.grid').length != 0){
+//         identifiedContainer = 'div.overflow-hidden'
+//     } else if ($('div.grid-images_box').length != 0) {
+//         identifiedContainer = 'div.grid-images'
+//     }
 
 
-    $(identifiedClass).each(function(e){
-        let item = []
-        let size, sizeMultiplier;
-        $(this).find('img').remove()
-        item.push($(this));
+//     $(identifiedClass).each(function(e){
+//         let item = []
+//         let size, sizeMultiplier;
+//         $(this).find('img').remove()
+//         item.push($(this));
 
-        if (debug || debugOnlyOne){console.log($(this))}
-        $(this).remove();
+//         if (debug || debugOnlyOne){console.log($(this))}
+//         $(this).remove();
 
-        let sizeInfo = $(this).find('p:eq(1)').text();
-        let sizeSplit = sizeInfo.split(" ");
-        size = parseFloat(sizeSplit[0]);
-        if (debug || debugOnlyOne) {console.log(sizeInfo + " " + size)}
+//         let sizeInfo = $(this).find('p:eq(1)').text();
+//         let sizeSplit = sizeInfo.split(" ");
+//         size = parseFloat(sizeSplit[0]);
+//         if (debug || debugOnlyOne) {console.log(sizeInfo + " " + size)}
 
-        let sizeMultiplierDeterminer = sizeSplit[1]
-        if (sizeMultiplierDeterminer == "KiB"){
-            sizeMultiplier = (1/1024);
-        } else if (sizeMultiplierDeterminer == "KB"){
-            sizeMultiplier = (1/1024);
-        } else if (sizeMultiplierDeterminer == "kB"){
-            sizeMultiplier = (1/1024);
-        }else if (sizeMultiplierDeterminer == "MiB"){
-            sizeMultiplier = 1;
-        } else if (sizeMultiplierDeterminer == "MB"){
-            sizeMultiplier = 1;
-        } else if (sizeMultiplierDeterminer == "GiB"){
-            sizeMultiplier = 1024;
-        } else if (sizeMultiplierDeterminer == "GB"){
-            sizeMultiplier = 1024;
-        }
-        item.push(size * sizeMultiplier);
-        if (debug || debugOnlyOne) {console.log(size + " = " + size * sizeMultiplier)}
+//         let sizeMultiplierDeterminer = sizeSplit[1]
+//         if (sizeMultiplierDeterminer == "KiB"){
+//             sizeMultiplier = (1/1024);
+//         } else if (sizeMultiplierDeterminer == "KB"){
+//             sizeMultiplier = (1/1024);
+//         } else if (sizeMultiplierDeterminer == "kB"){
+//             sizeMultiplier = (1/1024);
+//         }else if (sizeMultiplierDeterminer == "MiB"){
+//             sizeMultiplier = 1;
+//         } else if (sizeMultiplierDeterminer == "MB"){
+//             sizeMultiplier = 1;
+//         } else if (sizeMultiplierDeterminer == "GiB"){
+//             sizeMultiplier = 1024;
+//         } else if (sizeMultiplierDeterminer == "GB"){
+//             sizeMultiplier = 1024;
+//         }
+//         item.push(size * sizeMultiplier);
+//         if (debug || debugOnlyOne) {console.log(size + " = " + size * sizeMultiplier)}
 
-        items.push(item);
-        $(this).remove();
-        debugOnlyOne = false;
-    });
+//         items.push(item);
+//         $(this).remove();
+//         debugOnlyOne = false;
+//     });
 
-    var sortedItems = items.sort(function(a, b) {
-        return b[1] - a[1];
-    });
+//     var sortedItems = items.sort(function(a, b) {
+//         return b[1] - a[1];
+//     });
 
-    var newList = $('<ul></ul>')
-    for (let i=0; i < sortedItems.length; i++){
-        // $(identifiedContainer).append(sortedItems[i][0]);
-        newList.append(sortedItems[i][0]);
-    }
-    newList.insertAfter(identifiedContainer)
-    $(identifiedContainer).remove()
-    $('.grid-images_box-link').css('height','50px')
-})
+//     var newList = $('<ul></ul>')
+//     for (let i=0; i < sortedItems.length; i++){
+//         // $(identifiedContainer).append(sortedItems[i][0]);
+//         newList.append(sortedItems[i][0]);
+//     }
+//     newList.insertAfter(identifiedContainer)
+//     $(identifiedContainer).remove()
+//     $('.grid-images_box-link').css('height','50px')
+// })
