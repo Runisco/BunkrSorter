1dde1f6502e80173621e7b26d3d89f0d2d768a9e
Runisco <runisco@gmail.com>
2024-06-12 07:47:50 +0200

    Update BunkrSorter.user.js

--- BunkrSorter.user.js
+++ BunkrSorter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BunkrSorter
 // @namespace    https://github.com/runisco
-// @version      3.5
+// @version      3.6
 // @updateURL    https://github.com/Runisco/BunkrSorter/raw/main/BunkrSorter.user.js
 // @downloadURL  https://github.com/Runisco/BunkrSorter/raw/main/BunkrSorter.user.js
 // @supportURL   https://github.com/Runisco/BunkrSorter/issues
@@ -17,6 +17,7 @@
 // @match        https://bunkr.si/a/*
 // @match        https://bunkr.ws/a/*
 // @match        https://bunkr.se/a/*
+// @match        https://bunkr.fi/a/*
 // @match        https://bunkr.black/a/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bunkr.is
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
